b11ec8c332683f192e8ae46a22bdc60b5a158e82
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-27 19:07:22 +0100

    add test

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/comptime/cross-module-imported/output/ad3e4_tests_snapshot_comptime_cross-module-imported_input_index_afa01604.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    "output/ad3e4_tests_snapshot_comptime_cross-module-imported_input_index_afa01604.js",
+    {"otherChunks":["output/780ce_turbopack-tests_tests_snapshot_comptime_cross-module-imported_input_b966c626._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/comptime/cross-module-imported/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
