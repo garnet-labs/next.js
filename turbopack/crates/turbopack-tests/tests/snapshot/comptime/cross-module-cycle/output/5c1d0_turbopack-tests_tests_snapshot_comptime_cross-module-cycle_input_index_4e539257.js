445ea988681a7942fb6184be536556eb5f3d141d
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-03-02 21:29:34 +0100

    f cycle

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/comptime/cross-module-cycle/output/5c1d0_turbopack-tests_tests_snapshot_comptime_cross-module-cycle_input_index_4e539257.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    "output/5c1d0_turbopack-tests_tests_snapshot_comptime_cross-module-cycle_input_index_4e539257.js",
+    {"otherChunks":["output/780ce_turbopack-tests_tests_snapshot_comptime_cross-module-cycle_input_418e833a._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/comptime/cross-module-cycle/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
